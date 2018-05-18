bbedfd8fd3c715f558a488c93d78ba3e6448ecbe
amirhosein_kashani <amkkashani@gmail.com>
2018-05-18 12:46:14 +0430

    "before scrollbar"

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        MyFrame myFrame= new MyFrame();
+        MyFrame myFrame = new MyFrame();
+//        ScroolTesting testing=new ScroolTesting();
     }
 }
